82432af44332e035e9805c8b99f172c38be9508d
John Hobbs <john@velvetcache.org>
2010-12-01 10:47:23 -0600

    Force the display of the actual 404 on errors in IE and Chrome.

--- application/views/error/404.php
+++ application/views/error/404.php
@@ -3,3 +3,12 @@
 <p>
 	Not sure what you were looking for, but we can't find it!
 </p>
+
+<!-- Padding to Disable MSIE and Chrome default error pages -->
+<!-- Padding to Disable MSIE and Chrome default error pages -->
+<!-- Padding to Disable MSIE and Chrome default error pages -->
+<!-- Padding to Disable MSIE and Chrome default error pages -->
+<!-- Padding to Disable MSIE and Chrome default error pages -->
+<!-- Padding to Disable MSIE and Chrome default error pages -->
+<!-- Padding to Disable MSIE and Chrome default error pages -->
+<!-- Padding to Disable MSIE and Chrome default error pages -->
